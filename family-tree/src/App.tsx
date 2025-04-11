1a562a28480373bcdea95f658b10ba7a17f93fbc
Ksusha <https://github.com/ksupiska>
2025-04-11 16:50:32 +0300

    добавлен хедер вер.1

--- family-tree/src/App.tsx
+++ family-tree/src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import {  HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import {  HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import FamilyTree from './pages/FamilyTree';
 
+import Header from './components/Header';
 function App() {
 
 
   return (
     <>
       <Router>
-        <nav>
-          <Link to="/">Главная</Link>
-          <Link to="/family-tree">Семейное дерево</Link>
-        </nav>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/family-tree" element={<FamilyTree />} />
